23266f47dc29dee20e5bf763acb5eb87d6a179d3
thais-reiss <thaisthatareis@gmail.com>
2025-11-06 14:40:40 -0300

    feat: class node

---
+++ node.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, position, parent=None):
+        self.g = 0  
+        self.h = 0  
+        self.f = 0  
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+    def __hash__(self):
+        return hash(self.position)
+
+    def __lt__(self, other):
+        return self.f < other.f
